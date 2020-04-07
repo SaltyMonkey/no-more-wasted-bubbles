43aee93a44f874d78918908554cd520c2fac8b80
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-04-07 23:34:06 +0300

    update commands and added response from commands

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ class noMoreBubbles {
 		let npcs = new Set();
 
 		mod.command.add("bl", {
-			$default() { mod.settings.enabled = !mod.settings.enabled; },
-			pets() { mod.settings.onlyPets = !mod.settings.onlyPets; }
+			$default() { 
+				mod.settings.enabled = !mod.settings.enabled;
+				mod.command.message(`Mod was ${mod.settings.enabled ? "enabled": "disabled"}`);
+			},
+			pets() { 
+				mod.settings.onlyPets = !mod.settings.onlyPets;
+				mod.command.message(`Block bubbles status: ${mod.settings.onlyPets ? "only companions": "everything"}`);
+			}
 		});
 
 		mod.hook("S_SPAWN_NPC", 11, (event)=> { 
